1a2c0cf7d336ae8d9e790467a4a4be78dd171065
MaddyLobb <81719007+MaddyLobb@users.noreply.github.com>
2022-01-25 13:56:44 -0500

    edits

--- dev/js/simplemenu.js
+++ dev/js/simplemenu.js
@@ -4,6 +4,7 @@ gsap.set("#arrow",{transformOrigin:"center"});
 
 const arrowTL = new gsap.timeline();
 arrowTL.from("#arrow",{duration:0.5, rotate: 180});
+arrowTL.to("#arrow",{duration:0.25, stroke: "black"});
 
 export const navTL = new gsap.timeline({paused:true});
 
